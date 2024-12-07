e0567b530e826dc938f58369ea693abd33b640df
Neehar Kondapaneni <nkondapa@caltech.edu>
2024-12-06 17:06:53 -0800

    add setup stuff

---
+++ setup.sh
@@ -0,0 +1,5 @@
+apt-get update && apt-get install nano zip ffmpeg libsm6 libxext6 unar vim htop unzip gcc curl g++ python3-distutils python3-apt -y
+
+pip install -r requirements.txt
+
+python3 setup.py build_ext --inplace
