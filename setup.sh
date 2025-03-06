103ec8f7eb6db391400d906d4e8e6498b3498ed6
nkondapa <nkondapa@users.noreply.github.com>
2025-03-06 00:27:51 -0800

    Update setup.sh
    update setup script to not install ubuntu specific packages by default

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-apt-get update && apt-get install nano zip ffmpeg libsm6 libxext6 unar vim htop unzip gcc curl g++ python3-distutils python3-apt -y
+# uncomment if needed on your machine
+# apt-get update && apt-get install nano zip ffmpeg libsm6 libxext6 unar vim htop unzip gcc curl g++ python3-distutils python3-apt -y 
 
 pip install -r requirements.txt
 
