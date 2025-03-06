a38a932b1eda863dc29afb8677d036a11b6909ff
Neehar Kondapaneni <nkondapa@caltech.edu>
2025-03-05 18:02:46 -0800

    delete unnecessary code for toy concept experiment add gitkeep add readme change resnet comparison name

---
+++ README.md
@@ -0,0 +1,51 @@
+# Representational Similarity via Interpretable Concepts (RSVC)
+
+[![OpenReview](https://img.shields.io/badge/OpenReview-a61717)](https://openreview.net/forum?id=ih3BJmIZbC)
+
+
+### Setup
+```sh
+
+conda create -n "RSVC" python=3.8.18
+conda activate RSVC
+bash setup.sh
+
+```
+
+### Downloading Datasets
+1) ImageNet: You can download imagenet with ```bash download_imagenet.sh```.
+2) NABirds: Please visit https://dl.allaboutbirds.org/nabirds to request access and download the dataset.
+
+The datasets should be placed (or symlinked) in the `data/` directory.
+The structure should look like this:
+```
+data/
+│-- imagenet/
+│   │-- train/
+│   │-- val/
+│   │-- ILSVRC2012_devkit_t12/
+│   │-- ...
+|-- nabirds/
+    |-- images/
+    |-- parts/
+    |-- ...
+```
+
+## Usage
+### Toy Concept Comparison
+To run the toy concept comparison experiment, make sure the nabirds dataset is downloaded.
+You can download the custom trained models from [$M_{ps}$](https://drive.google.com/drive/folders/1oG6uHMPahBYVc-AtqVYSZyWAvq8u74zw?usp=drive_link) and
+[$M_{nc}$](https://drive.google.com/drive/folders/1LPXUh_Q3J9CCAg__o-T3RAcoVAAQ8ZRe?usp=drive_link).
+
+```bash toy_concept_experiment.sh```
+
+### RN18 vs. RN50
+To run the resnet size comparison make sure the imagenet dataset is downloaded and placed in the correct location.
+These models should automatically download from the timm library.
+
+```bash compare_rn18_rn50.sh```
+
+## TODO
+- [ ] Add qualitative visualization code to resnet comparisons
+- [ ] Add layerwise comparison visualizations
+- [ ] Add remaining comparison experiments
--- compare_rn18_v_rn50.sh
+++ compare_rn18_v_rn50.sh
@@ -1,28 +1,28 @@
 
-python extract_model_activations.py --model resnet18.a2_in1k --feature_layer_version v1 --output_root /media/nkondapa/SSD2/RSVC/ --start_class_idx 0 --end_class_idx 5
-python extract_model_activations.py --model resnet50.a2_in1k --feature_layer_version v1 --output_root /media/nkondapa/SSD2/RSVC/ --start_class_idx 0 --end_class_idx 5
+python extract_model_activations.py --model resnet18.a2_in1k --feature_layer_version v1 --output_root /media/nkondapa/SSD2/RSVC/ --start_class_idx 0 --end_class_idx 100
+python extract_model_activations.py --model resnet50.a2_in1k --feature_layer_version v1 --output_root /media/nkondapa/SSD2/RSVC/ --start_class_idx 0 --end_class_idx 100
 
-python extract_concepts.py --model resnet18.a2_in1k --feature_layer_version v1 --output_root /media/nkondapa/SSD2/concept_book/ --start_class_idx 0 --end_class_idx 5
-python extract_concepts.py --model resnet50.a2_in1k --feature_layer_version v1 --output_root /media/nkondapa/SSD2/concept_book/ --start_class_idx 0 --end_class_idx 5
+python extract_concepts.py --model resnet18.a2_in1k --feature_layer_version v1 --output_root /media/nkondapa/SSD2/concept_book/ --start_class_idx 0 --end_class_idx 100
+python extract_concepts.py --model resnet50.a2_in1k --feature_layer_version v1 --output_root /media/nkondapa/SSD2/concept_book/ --start_class_idx 0 --end_class_idx 100
 
 python compare_models.py --comparison_config ./comparison_configs/r18_flv=v1_vs_r50_flv=v1-nmf=10_seed=0.json \
---start_class_idx 0 --end_class_idx 5 --folder_exists overwrite \
+--start_class_idx 0 --end_class_idx 100 --folder_exists overwrite \
 --concept_root_folder_0 /media/nkondapa/SSD2/concept_book/ --concept_root_folder_1 /media/nkondapa/SSD2/concept_book/
 
 python evaluate_regression.py --comparison_config ./comparison_configs/r18_flv=v1_vs_r50_flv=v1-nmf=10_seed=0.json \
 --concept_root_folder_0 /media/nkondapa/SSD2/concept_book/ --concept_root_folder_1 /media/nkondapa/SSD2/concept_book/ \
---start_class_idx 0 --end_class_idx 5 \
+--start_class_idx 0 --end_class_idx 100 \
 --data_split val --patchify
 
 python concept_integrated_gradients.py --comparison_config ./comparison_configs/r18_flv=v1_vs_r50_flv=v1-nmf=10_seed=0.json \
---start_class_idx 0 --end_class_idx 5 \
+--start_class_idx 0 --end_class_idx 100 \
 --model_0 resnet18.a2_in1k --model_1 resnet50.a2_in1k \
 --concept_root_folder_0 /media/nkondapa/SSD2/concept_book/ --concept_root_folder_1 /media/nkondapa/SSD2/concept_book/
 
 python replacement_test.py --comparison_config ./comparison_configs/r18_flv=v1_vs_r50_flv=v1-nmf=10_seed=0.json \
---start_class_idx 0 --end_class_idx 5 \
+--start_class_idx 0 --end_class_idx 100 \
 --concept_root_folder_0 /media/nkondapa/SSD2/concept_book/ --concept_root_folder_1 /media/nkondapa/SSD2/concept_book/
 
 python visualize_similarity_vs_importance.py --comparison_config ./comparison_configs/r18_flv=v1_vs_r50_flv=v1-nmf=10_seed=0.json \
---start_class_idx 0 --end_class_idx 5 \
+--start_class_idx 0 --end_class_idx 100 \
 --concept_root_folder_0 /media/nkondapa/SSD2/concept_book/ --concept_root_folder_1 /media/nkondapa/SSD2/concept_book/
